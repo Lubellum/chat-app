05e7a8aa0180e3e1c35d2e4436c497398e87e107
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-10-06 14:07:37 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,3 @@
-# class Message < ApplicationRecord
-#   belongs_to :room
-#   belongs_to :user
-#   has_one_attached :image
-
-#   validates :content, presence: true
-# end
-
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
